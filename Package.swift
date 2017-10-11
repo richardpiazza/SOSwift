992967690acaa2819b83f6056b521470ec0c8c4d
Richard Piazza <richard@richardpiazza.com>
2017-10-11 13:04:38 -0500

    Swift 4 Package

--- Package.swift
+++ Package.swift
@@ -1,35 +1,18 @@
-//===----------------------------------------------------------------------===//
-//
-// Package.swift
-//
-// Copyright (c) 2016 Richard Piazza
-// https://github.com/richardpiazza/SOSwift
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-//
-//===----------------------------------------------------------------------===//
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SOSwift",
+    products: [
+        .library(name: "SOSwift", targets: ["SOSwift"]),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/richardpiazza/SOSwiftVocabulary", majorVersion: 0, minor: 1)
+        .package(url: "https://github.com/richardpiazza/SOSwiftVocabulary", .upToNextMajor(from: "0.1.1")),
+    ],
+    targets: [
+        .target(name: "SOSwift", dependencies: ["SOSwiftVocabulary"], path: "Sources"),
+        .testTarget(name: "SOSwiftTests", dependencies: ["SOSwift"], path: "Tests")
     ]
 )
