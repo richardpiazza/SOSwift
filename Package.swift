12d285b23af94b6db6093a96d8ef7e7de39d0b24
Richard Piazza <richard@richardpiazza.com>
2018-02-20 13:30:20 -0600

    Deprecated the ObjC Implementation (same as SOSwiftVocabulary).

--- Package.swift
+++ Package.swift
@@ -7,14 +7,12 @@ let package = Package(
     name: "SOSwift",
     products: [
         .library(name: "SOSwift", targets: ["SOSwift"]),
-        .library(name: "SOSwift-ObjC", targets: ["SOSwift-ObjC"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/richardpiazza/SOSwiftVocabulary", .upToNextMinor(from: "1.3.0"))
+        .package(url: "https://github.com/richardpiazza/SOSwiftVocabulary", .upToNextMinor(from: "1.4.0"))
     ],
     targets: [
         .target(name: "SOSwift", dependencies: ["SOSwiftVocabulary"], path: "Sources/SOSwift"),
         .testTarget(name: "SOSwiftTests", dependencies: ["SOSwift"], path: "Tests/SOSwiftTests"),
-        .target(name: "SOSwift-ObjC", dependencies: ["SOSwiftVocabulary-ObjC"], path: "Sources/SOSwift-ObjC"),
     ]
 )
