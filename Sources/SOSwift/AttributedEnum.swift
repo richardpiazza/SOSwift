9e6183ee8711edc80c528fda680ea28478832367
Richard Piazza <richard@richardpiazza.com>
2018-05-19 07:24:09 -0500

    Adjusted AttributedEnum to remove RawRepresentable Conformance (issues with associatedType requirements).

--- Sources/SOSwift/AttributedEnum.swift
+++ Sources/SOSwift/AttributedEnum.swift
@@ -1,12 +1,26 @@
 import Foundation
 
-public protocol AttributedEnum: RawRepresentable {
+public protocol AttributedEnum {
     static var allCases: [Self] { get }
+    static var displayNames: [String] { get }
     var displayName: String { get }
+    init?(displayName: String)
 }
 
 public extension AttributedEnum {
-    init?(displayName: String) {
+    public static func attributedEnumType(displayName: String) -> Self? {
+        guard let type = self.init(displayName: displayName) else {
+            return nil
+        }
+        
+        return type
+    }
+    
+    public static var displayNames: [String] {
+        return allCases.map({ return $0.displayName })
+    }
+    
+    public init?(displayName: String) {
         let match = Self.allCases.first { (`case`) -> Bool in
             return `case`.displayName == displayName
         }
