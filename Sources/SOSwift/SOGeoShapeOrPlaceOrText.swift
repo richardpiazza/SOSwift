deea4276c045d72fba6bad07c06208749c7c7076
Richard Piazza <richard@richardpiazza.com>
2019-06-17 19:36:56 -0500

    SOGeoShapeOrPlaceOrText implementation and tests.

--- Sources/SOSwift/SOGeoShapeOrPlaceOrText.swift
+++ Sources/SOSwift/SOGeoShapeOrPlaceOrText.swift
@@ -1,10 +1,100 @@
 import Foundation
 import SOSwiftVocabulary
 
-// MARK: - GeoShapeOrPlaceOrText
+public enum SOGeoShapeOrPlaceOrText: GeoShapeOrPlaceOrText, Codable {
+    case geoShape(value: SOGeoShape)
+    case place(value: SOPlace)
+    case text(value: String)
+    
+    public init(from decoder: Decoder) throws {
+        var dictionary: [String : Any]?
+        
+        do {
+            let jsonContainer = try decoder.container(keyedBy: JSONCodingKeys.self)
+            dictionary = try jsonContainer.decode(Dictionary<String, Any>.self)
+        } catch {
+            
+        }
+        
+        guard let jsonDictionary = dictionary else {
+            let container = try decoder.singleValueContainer()
+            let value = try container.decode(String.self)
+            self = .text(value: value)
+            return
+        }
+        
+        guard let type = jsonDictionary[SOThing.Keywords.type] as? String else {
+            throw DynamicError.invalidTypeKey
+        }
+        
+        let container = try decoder.singleValueContainer()
+        
+        switch type {
+        case SOGeoShape.type:
+            let value = try container.decode(SOGeoShape.self)
+            self = .geoShape(value: value)
+        case SOPlace.type:
+            let value = try container.decode(SOPlace.self)
+            self = .place(value: value)
+        default:
+            throw DynamicError.invalidTypeKey
+        }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        
+        switch self {
+        case .geoShape(let value):
+            try container.encode(value)
+        case .place(let value):
+            try container.encode(value)
+        case .text(let value):
+            try container.encode(value)
+        }
+    }
+    
+    public var geoShape: SOGeoShape? {
+        switch self {
+        case .geoShape(let value):
+            return value
+        default:
+            return nil
+        }
+    }
+    
+    public var place: SOPlace? {
+        switch self {
+        case .place(let value):
+            return value
+        default:
+            return nil
+        }
+    }
+    
+    public var text: String? {
+        switch self {
+        case .text(let value):
+            return value
+        default:
+            return nil
+        }
+    }
+}
+
+// MARK: - Encoding
 
 public extension KeyedEncodingContainer {
     mutating func encodeIfPresent(_ value: GeoShapeOrPlaceOrText?, forKey key: K) throws {
+        guard value != nil else {
+            return
+        }
+        
+        if let typedValue = value as? SOGeoShapeOrPlaceOrText {
+            try self.encode(typedValue, forKey: key)
+            return
+        }
+        
         if let typedValue = value as? SOGeoShape {
             try self.encode(typedValue, forKey: key)
         } else if let typedValue = value as? SOPlace {
@@ -13,7 +103,32 @@ public extension KeyedEncodingContainer {
             try self.encode(typedValue, forKey: key)
         }
     }
+    
+    mutating func encodeIfPresent(_ value: [GeoShapeOrPlaceOrText]?, forKey key: K) throws {
+        guard value != nil else {
+            return
+        }
+        
+        if let typedValue = value as? [SOGeoShapeOrPlaceOrText] {
+            try self.encode(typedValue, forKey: key)
+            return
+        }
+        
+        var container = nestedUnkeyedContainer(forKey: key)
+        
+        try value?.forEach({ (object) in
+            if let typedValue = object as? SOGeoShape {
+                try container.encode(typedValue)
+            } else if let typedValue = object as? SOPlace {
+                try container.encode(typedValue)
+            } else if let typedValue = object as? String {
+                try container.encode(typedValue)
+            }
+        })
+    }
 }
+
+// MARK: - Decoding
 
 public extension KeyedDecodingContainer {
     func decodeGeoShapeOrPlaceOrTextIfPresent(forKey key: K) throws -> GeoShapeOrPlaceOrText? {
@@ -22,23 +137,21 @@ public extension KeyedDecodingContainer {
         }
         
         do {
-            let dictionary = try self.decode(Dictionary<String, Any>.self, forKey: key)
-            if dictionary[SOThing.Keywords.type] as? String == SOGeoShape.type {
-                return try self.decode(SOGeoShape.self, forKey: key)
-            } else if dictionary[SOThing.Keywords.type] as? String == SOThing.type {
-                return try self.decode(SOPlace.self, forKey: key)
-            }
+            return try self.decodeIfPresent(SOGeoShapeOrPlaceOrText.self, forKey: key)
         } catch {
+            return nil
+        }
+    }
+    
+    func decodeGeoShapesOrPlacesOrTextsIfPresent(forKey key: K) throws -> [GeoShapeOrPlaceOrText]? {
+        guard self.contains(key) else {
+            return nil
         }
         
         do {
-            let value = try self.decode(String.self, forKey: key)
-            return value
+            return try self.decodeIfPresent([SOGeoShapeOrPlaceOrText].self, forKey: key)
         } catch {
+            return nil
         }
-        
-        print("Failed to decode 'GeoShapeOrPlaceOrText' for key: \(key.stringValue).")
-        
-        return nil
     }
 }
---
+++ Tests/SOSwiftTests/GeoShapeOrPlaceOrTextTests.swift
@@ -0,0 +1,165 @@
+import XCTest
+@testable import SOSwift
+import SOSwiftVocabulary
+
+class GeoShapeOrPlaceOrTextTests: XCTestCase {
+    
+    static var allTests = [
+        ("testSingleDecodes", testSingleDecodes),
+        ("testSingleEncodes", testSingleEncodes),
+        ("testMultipleDecodes", testMultipleDecodes),
+        ("testMultipleEncodes", testMultipleEncodes),
+    ]
+    
+    fileprivate class TestClass: Codable, Testable {
+        var geoShape: GeoShapeOrPlaceOrText?
+        var place: GeoShapeOrPlaceOrText?
+        var text: GeoShapeOrPlaceOrText?
+        var multiple: [GeoShapeOrPlaceOrText]?
+        
+        private enum CodingKeys: String, CodingKey {
+            case geoShape
+            case place
+            case text
+            case multiple
+        }
+        
+        init() {
+            
+        }
+        
+        required init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            geoShape = try container.decodeGeoShapeOrPlaceOrTextIfPresent(forKey: .geoShape)
+            place = try container.decodeGeoShapeOrPlaceOrTextIfPresent(forKey: .place)
+            text = try container.decodeGeoShapeOrPlaceOrTextIfPresent(forKey: .text)
+            multiple = try container.decodeGeoShapesOrPlacesOrTextsIfPresent(forKey: .multiple)
+        }
+        
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try container.encodeIfPresent(geoShape, forKey: .geoShape)
+            try container.encodeIfPresent(place, forKey: .place)
+            try container.encodeIfPresent(text, forKey: .text)
+            try container.encodeIfPresent(multiple, forKey: .multiple)
+        }
+    }
+    
+    func testSingleDecodes() throws {
+        let json = """
+        {
+            "geoShape": {
+                "@type": "GeoShape",
+                "name": "Squircle"
+            },
+            "place": {
+                "@type": "Place",
+                "name": "Seattle"
+            },
+            "text": "Remember The Zune"
+        }
+        """
+        
+        let testObject = try TestClass.make(with: json)
+        
+        let geoShape = (testObject.geoShape as? SOGeoShapeOrPlaceOrText)?.geoShape
+        let place = (testObject.place as? SOGeoShapeOrPlaceOrText)?.place
+        let text = (testObject.text as? SOGeoShapeOrPlaceOrText)?.text
+        
+        XCTAssertEqual(geoShape?.name, "Squircle")
+        XCTAssertEqual(place?.name, "Seattle")
+        XCTAssertEqual(text, "Remember The Zune")
+    }
+    
+    func testSingleEncodes() throws {
+        let geoShape = SOGeoShape()
+        geoShape.name = "Squircle"
+        
+        let place = SOPlace()
+        place.name = "Seattle"
+        
+        let text = "Remember The Zune"
+        
+        let testObject = TestClass()
+        testObject.geoShape = SOGeoShapeOrPlaceOrText.geoShape(value: geoShape)
+        testObject.place = SOGeoShapeOrPlaceOrText.place(value: place)
+        testObject.text = SOGeoShapeOrPlaceOrText.text(value: text)
+        
+        let dictionary = try testObject.dictionary()
+        
+        let gs = dictionary["geoShape"] as? [String : Any]
+        let p = dictionary["place"] as? [String : Any]
+        let t = dictionary["text"] as? String
+        
+        XCTAssertEqual(gs?["name"] as? String, geoShape.name)
+        XCTAssertEqual(p?["name"] as? String, place.name)
+        XCTAssertEqual(t, text)
+    }
+    
+    func testMultipleDecodes() throws {
+        let json = """
+        {
+            "multiple": [
+                {
+                    "@type": "GeoShape",
+                    "name": "Squircle"
+                },
+                {
+                    "@type": "Place",
+                    "name": "Seattle"
+                },
+                "Remember The Zune"
+            ]
+        }
+        """
+        
+        let testObject = try TestClass.make(with: json)
+        
+        guard let multiple = testObject.multiple as? [SOGeoShapeOrPlaceOrText] else {
+            XCTFail()
+            return
+        }
+        
+        XCTAssertEqual(multiple.count, 3)
+        
+        let geoShape = multiple[0].geoShape
+        let place = multiple[1].place
+        let text = multiple[2].text
+        
+        XCTAssertEqual(geoShape?.name, "Squircle")
+        XCTAssertEqual(place?.name, "Seattle")
+        XCTAssertEqual(text, "Remember The Zune")
+    }
+    
+    func testMultipleEncodes() throws {
+        let geoShape = SOGeoShape()
+        geoShape.name = "Squircle"
+        
+        let place = SOPlace()
+        place.name = "Seattle"
+        
+        let text = "Remember The Zune"
+        
+        var multiple = [SOGeoShapeOrPlaceOrText]()
+        multiple.append(.geoShape(value: geoShape))
+        multiple.append(.place(value: place))
+        multiple.append(.text(value: text))
+        
+        let testObject = TestClass()
+        testObject.multiple = multiple
+        
+        let dictionary = try testObject.dictionary()
+        
+        let values = dictionary["multiple"] as? [Any]
+        
+        XCTAssertEqual(values?.count, 3)
+        
+        let gs = values?[0] as? [String : Any]
+        let p = values?[1] as? [String : Any]
+        let t = values?[2] as? String
+        
+        XCTAssertEqual(gs?["name"] as? String, geoShape.name)
+        XCTAssertEqual(p?["name"] as? String, place.name)
+        XCTAssertEqual(t, text)
+    }
+}
